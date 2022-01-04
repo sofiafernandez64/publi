d11d2586a22c061d2d72c6c9de0335554c8fca3e
Alumno <Alumno@EM44706182>
2022-01-03 23:55:23 -0300

    gt

--- primer_proyectoFernandez/src/main/java/ar/edu/unju/fi/tp/ejemplos/PrimerProyectoFernandezApplication.java
+++ primer_proyectoFernandez/src/main/java/ar/edu/unju/fi/tp/ejemplos/PrimerProyectoFernandezApplication.java
@@ -8,7 +8,7 @@ public class PrimerProyectoFernandezApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimerProyectoFernandezApplication.class, args);
-	System.out.println("b");
+	System.out.println("bi");
 	}
 
 }
